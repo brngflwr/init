d9c6534544429f6fab1e8c60100016faffd0ec80
brngflwr <86535875+brngflwr@users.noreply.github.com>
2021-06-26 17:34:14 -0400

    Create homebrew-vPlayer.sh

---
+++ homebrew-vPlayer.sh
@@ -0,0 +1,7 @@
+echo "~# Homebrew Secret Sauce 🥘"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+sleep 4  # sleeping for 4 seconds is important work
+
+echo "~# Mplayer 📽"
+brew install mplayer
